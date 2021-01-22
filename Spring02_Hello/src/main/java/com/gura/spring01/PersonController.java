09dae0262fc32d7300932ab1be890575153b9fe9
journey <tc02117@naver.com>
2021-01-22 11:52:14 +0900

    Spring02 updated, Spring03_ModelAndView added

--- Spring02_Hello/src/main/java/com/gura/spring01/PersonController.java
+++ Spring02_Hello/src/main/java/com/gura/spring01/PersonController.java
@@ -5,15 +5,19 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/* component scan 될 때 PersonController 객체가 생성 되고 관리 된다 */
 @Controller
 public class PersonController {
 	
-	@RequestMapping("/person.do")
+	@RequestMapping("/person.do") //.do 생략 가능
 	public String person(HttpServletRequest request) {
+		//view page에 전달할 data (Model)
 		String personToday="이걸 보는 당신!";
+		//request scope에 담는다
 		request.setAttribute("personToday", personToday);
 		
-		return "person";
+		//forward 이동할 view page의 위치를 리턴해준다
+		return "person"; // /WEB-INF/view/person.jsp
 	}
 	
 }
--- Spring02_Hello/src/main/webapp/WEB-INF/views/person.jsp
+++ Spring02_Hello/src/main/webapp/WEB-INF/views/person.jsp
@@ -4,10 +4,11 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>/views/person.jsp</title>
 </head>
 <body>
 <h1>오늘의 인물 페이지</h1>
 <p>오늘의 인물: ${requestScope.personToday }</p>
+<a href="home.do">인덱스로 가기</a>
 </body>
 </html>
