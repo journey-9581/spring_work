9858577791bb03061c659923924d59e04ad7d5ba
journey <tc02117@naver.com>
2021-01-27 12:33:16 +0900

    Spring05 updated: login

--- Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
@@ -1,7 +1,7 @@
 package com.gura.spring05.users.controller;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,24 +20,35 @@ public class UsersController {
 	@Autowired
 	private UsersService service;
 	
+	@RequestMapping(value = "/users/login", method = RequestMethod.POST)
+	public String login(HttpServletRequest request,
+			HttpServletResponse response) {
+		//로그인에 관련된 로직을 서비스를 통해서 처리한다.
+		service.loginLogic(request, response);
+		//view page  로 forward  이동해서 응답
+		return "users/login";
+	}
+	
 	//로그인 폼 요청 처리
 	@RequestMapping("/users/loginform")
-	public ModelAndView loginform(HttpServletRequest request, ModelAndView mView) {
-		//로그인폼에 관련된 로직을 서비스를 통해서 처리한다
+	public ModelAndView loginform(HttpServletRequest request, 
+			ModelAndView mView) {
+		//로그인 폼에 관련된 로직을 서비스를 통해서 처리한다.
 		service.loginformLogic(request, mView);
-		//view page 정보도 담는다
+		//view page 정보도 담는다.
 		mView.setViewName("users/loginform");
 		//리턴
 		return mView;
 	}
 	
-	//회원 가입 요청 처리
-	//form 전송은 보통 post 방식 요청인데 post 방식 요청만 받아들이도록 컨트롤러에 설정하는게 일반적이다
+	//회원 가입 요청처리
+	//form 전송은 보통 post 방식 요청인데 post 방식 요청만 받아들이도록 
+	//컨트롤러에 설정하는게 일반적이다. 
 	@RequestMapping(value = "/users/signup", method = RequestMethod.POST)
 	public String signup(@ModelAttribute("dto") UsersDto dto) {
 		/*
-		 * Dto인 경우에 @ModelAttribute("key값")으로 설정하면
-		 * 해당 Dto가 request 영역에 설정한 "key값"으로 자동 저장 된다
+		 *  Dto 인경우에 @ModelAttribute("key  값") 으로 설정하면
+		 *  해당 Dto 가  request 영역에  설정한 "key 값" 으로 담긴다.
 		 */
 		service.addUser(dto);
 		return "users/signup";
@@ -47,17 +58,19 @@ public class UsersController {
 	public String signupForm() {
 		return "users/signup_form";
 	}
-	
-	//ajax 요청 처리
+	//ajax 요청 처리 
 	@RequestMapping("/users/checkid")
-	public ModelAndView checkid(@RequestParam String inputId, ModelAndView mView) {
+	public ModelAndView checkid(@RequestParam String inputId,
+			ModelAndView mView) {
 		/*
-		 * @RequestParam String inputId는
-		 * String inputId=request.getParameter("inputId")와 같다
+		 * (@RequestParam String inputId) 
+		 * 는
+		 * String inputId=request.getParameter("inputId") 
+		 * 와 같다.
 		 */
-		//서비스를 이용해서 해당 아이디가 존재하는지 여부를 알아낸다
+		//서비스를 이용해서 해당 아이디가 존재하는지 여부를 알아낸다.
 		boolean isExist=service.isExistId(inputId);
-		//ModelAndView 객체에 해당 정보를 담고 view page로 forward 이동해서 응답
+		//ModelAndView 객체에 해당 정보를 담고 view page 로 forward 이동해서 응답
 		mView.addObject("isExist", isExist);
 		mView.setViewName("users/checkid");
 		return mView;
